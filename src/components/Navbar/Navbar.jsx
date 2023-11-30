250f5eb8770e26a7d8f8959297be2ab24eeb0e56
Andriana <andrianasaffo@gmail.com>
2023-11-30 15:45:02 +0000

    Create navbar component and CSS
    Navbar created to with close button. Close handler passed in as a prop from header component.
    Menu items will be changed to the appropriate routes instead of the anchor tags they are now.
    CSS input to style close button and menu items. Colours will be changed to fit colour scheme

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = ({ handleClose }) => {
+  return (
+    <nav className="nav-menu">
+      <section className="close-icon" onClick={handleClose}>
+        <span className="menu-icon bar-one"></span>
+        <span className="menu-icon bar-two"></span>
+      </section>
+      <section className="menu-list">
+        <a href="/">Home</a>
+        <a href="/form">Graduate Form</a>
+        <a href="/about">About me</a>
+        <a href="/graduates">Meet the Graduates</a>
+      </section>
+    </nav>
+  );
+};
+
+export default Navbar;
