4da7c583ad74730c0906293325b37818ca5168a1
Andriana <andrianasaffo@gmail.com>
2023-11-30 20:30:04 +0000

    Landing page and footer component added
    Landing page and footer component added into app.js to be able to view progress so far

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <LandingPage />
+      <Footer/>
     </div>
   );
 }
