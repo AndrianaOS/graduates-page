f3f18da4876c75b76e0238d3ee365eaa4e8b150b
Andriana <andrianasaffo@gmail.com>
2023-12-02 16:18:04 +0000

    Code changed to reflect backend
    Backend code changed to combine github data and database info together. Result is mapped and displayed in the frontend.
    Each graduate box will show the graduate's github picture, their name, github bio, link to their cv, github link, email and linkedin link and their role. Preliminary CSS added to page for styling at 320px

--- src/pages/GraduatesPage/GraduatesPage.jsx
+++ src/pages/GraduatesPage/GraduatesPage.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 const GraduatesPage = () => {
   const [graduateData, setGraduateData] = useState([]);
+  //   const [graduateGithubData, setGraduateGithubData] = useState([]);
 
   async function getAllGraduates() {
     try {
@@ -22,24 +23,6 @@ const GraduatesPage = () => {
     }
   }
 
-  //   TO BE USED TO FETCH DATA FROM GITHUB GRAPHQL API
-  // async function getAllGraduatesGithub() {
-  //   try {
-  //     const response = await fetch("http://127.0.0.1:5000/allGraduates");
-
-  //     if (!response.ok) {
-  //       throw new Error(`Error: ${response.error}`);
-  //     }
-
-  //     const data = await response.json();
-  //     console.log(data);
-  //     setGraduateData(data);
-  //     return data;
-  //   } catch (error) {
-  //     console.log("Error message:", error);
-  //   }
-  // }
-
   useEffect(() => {
     getAllGraduates();
   }, []);
@@ -53,16 +36,35 @@ const GraduatesPage = () => {
         <div>
           {graduateData.length > 0 &&
             graduateData.map((graduate) => (
-              <aside key={graduate.id} className="graduate-data">
-                <div className="photo">Image</div>
-                <p>{graduate.name}</p>
-                <p>
-                  <a href={graduate.cv_link}>CV</a>
-                </p>
-                <p>
-                  <a href={graduate.github_url}>GitHub</a>
-                </p>
-                <p>{graduate.role} developer</p>
+              <aside key={graduate.db_data.id} className="graduate-data">
+                <img
+                  src={graduate.github_data.data.user.avatarUrl}
+                  alt={`${graduate.db_data.name} Avatar`}
+                />
+                <h2>{graduate.db_data.name}</h2>
+                <p className="bio">{graduate.github_data.data.user.bio}</p>
+                <section className="link-info">
+                  <p>
+                    <a href={graduate.db_data.cv_link}>CV</a>
+                  </p>
+                  <p>
+                    <a href={graduate.db_data.github_url}>GitHub</a>
+                  </p>
+                  {graduate.github_data.data.user.email && (
+                    <p>Email: {graduate.github_data.data.user.email}</p>
+                  )}
+                  <p>
+                    <a
+                      href={graduate.github_data.data.user.socialAccounts.nodes.map(
+                        (data) => data.url
+                      )}
+                    >
+                      LinkedIn
+                    </a>
+                  </p>
+                </section>
+
+                <p>{graduate.db_data.role} developer</p>
               </aside>
             ))}
         </div>
