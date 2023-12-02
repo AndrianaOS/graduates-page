939d021456491670750074bb3bf49c51e178a1f9
Andriana <andrianasaffo@gmail.com>
2023-12-02 14:43:19 +0000

    Create page for form
    This component will contain the graduate form that need to be filled to populate the graduate page

---
+++ src/pages/GraduateForm/GraduateForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const GraduateForm = () => {
+  return (
+    <div>
+      GraduateForm
+      <Link to="/">Back to Homepage</Link>
+    </div>
+  );
+};
+
+export default GraduateForm;
