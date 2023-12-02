92c0114248503fe27abc3e5fe888b8ebaedcd333
Andriana <andrianasaffo@gmail.com>
2023-12-02 14:51:15 +0000

    Graduate page added to app.js
    Added graduate page as a route to the application to be able to view graduates once button is clicked

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import GraduatesPage from "./pages/GraduatesPage/GraduatesPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/graduates" element={<GraduatesPage />} />
       </Routes>
       <Footer />
     </div>
