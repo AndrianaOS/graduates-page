bca3e48066ed45f8bfe15444cd6bdfe3fcbf43c9
Andriana <andrianasaffo@gmail.com>
2023-12-03 16:36:01 +0000

    input picture and style
    Folder created for images. Picture placed on landing page and styled

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./landingPage.css";
 import Button from "../../components/Button/Button";
+import landingPic from "../../images/landingPic.webp";
 
 const LandingPage = () => {
   return (
     <div className="homepage">
       <section className="info-area">
-        <aside className="picture-area">Place Image here</aside>
+        <aside className="picture-area">
+          <img src={landingPic} alt="group graduate pic" />
+        </aside>
         <aside className="page-desc">
           <p>
             Welcome to CodeYourFuture's Graduates' Page where you can view all
             our eligible graduates and either connect with them or view some of
             their wonderful work they have created.
+            <div className="form-info">
+              **NOTE: If you would like to be displayed, click button below and fill in
+              the form.
+            </div>
           </p>
         </aside>
       </section>
@@ -21,10 +28,6 @@ const LandingPage = () => {
           <Button buttonName={"See all our Graduates"} />
         </Link>
 
-        <span className="form-info">
-          If you would like to be displayed, click button below and fill in the
-          form.
-        </span>
         <Link to="/form" className="button-space">
           <Button buttonName={"Add Graduate"} />
         </Link>
